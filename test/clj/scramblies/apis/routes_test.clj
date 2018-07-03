cb97f5adf8759cff87454daf746e2d09cb4805ae
Manuel Uberti <manuel.uberti@inventati.org>
2018-07-03 16:45:47 +0200

    Add unit test for scramble function

--- test/clj/scramblies/apis/routes_test.clj
+++ test/clj/scramblies/apis/routes_test.clj
@@ -3,9 +3,16 @@
             [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [scramblies.apis.resources.scramble :as s]
+            [scramblies.apis.routes :as r]
             [scramblies.services.http :as http]))
 
-(deftest scramble-api
+(deftest scramble-test
+  (testing "Testing scramble function"
+    (with-redefs [s/scramble? (fn [s1 s2] true)]
+      (is (= (r/scramble {:s1 "testing" :s2 "test"})
+             {:scramble? true})))))
+
+(deftest scramble-route-test
   (testing "Testing scramble API endpoint"
     (with-redefs [s/scramble? (fn [s1 s2] true)]
       (let [body (json/generate-string {:s1 "testing" :s2 "test"})
